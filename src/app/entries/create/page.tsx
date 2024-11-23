ab64293665133c532368d11342cf1abf34782d81
ss49919201 <sakashin10291029@gmail.com>
2024-11-23 10:08:31 +0900

    add create entry action

--- src/app/entries/create/page.tsx
+++ src/app/entries/create/page.tsx
@@ -1,7 +1,9 @@
+import { createEntry } from "@/app/lib/action/createEntriy";
+
 export default async function Page() {
   return (
     <main>
-      <form action="" className="rounded-md bg-gray-50 p-4 md:p-6">
+      <form action={createEntry} className="rounded-md bg-gray-50 p-4 md:p-6">
         <div className="mb-4">
           <div className="mb-4">
             <label htmlFor="title">Title</label>
@@ -26,7 +28,7 @@ export default async function Page() {
           </div>
           <div className="mb-4">
             <button
-              type="button"
+              type="submit"
               className="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Submit
---
+++ src/app/lib/action/createEntriy.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export async function createEntry(data: FormData) {
+  console.log(data);
+}
