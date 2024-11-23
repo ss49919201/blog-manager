88b46e450dde1457528eb260908f71451ba8218f
ss49919201 <sakashin10291029@gmail.com>
2024-11-23 14:58:25 +0900

    add parse and redirect

--- src/app/lib/action/createEntriy.ts
+++ src/app/lib/action/createEntriy.ts
@@ -1,5 +1,22 @@
 "use server";
 
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const schema = z.object({
+  title: z.string().min(1),
+  body: z.string().min(1),
+});
+
 export async function createEntry(data: FormData) {
   console.log(data);
+
+  const { title, body } = schema.parse({
+    title: data.get("title"),
+    body: data.get("body"),
+  });
+
+  console.log(title, body);
+
+  redirect("/entries");
 }
